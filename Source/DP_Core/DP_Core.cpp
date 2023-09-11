1b0cd968b1d64e4eb8a8bbf5ee88c6e28a69b7e5
MukshinV <mega.volter.dev@gmail.com>
2023-09-11 03:53:45 +0300

    Initial Project

---
+++ Source/DP_Core/DP_Core.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DP_Core.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DP_Core, "DP_Core" );
---
+++ Source/DP_Core/DP_CoreGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DP_CoreGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DP_CORE_API ADP_CoreGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
